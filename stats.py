8ce7ec2dd3cd7032877f489b4f4c28bf9d54435e
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2022-07-13 23:57:24 +0530

    print a graphic based on post frequencies

--- stats.py
+++ stats.py
@@ -1,4 +1,9 @@
+import datetime
 from bs4 import BeautifulSoup
+
+month_lengths = {
+    1: 31, 2: 28, 3:31, 4: 30, 5: 31, 6:30, 7:31, 8:31, 9:30, 10:31, 11:30, 12:31
+}
 
 def extract_date_posts():
     file_contents = None
@@ -21,10 +26,37 @@ def extract_date_posts():
             posts[date_in_consideration]  = list(map(lambda link: link.get_text().strip(), div.find_all('a')))
     return posts
 
+def next_day():
+    start_date = datetime.date(2021, 1, 1)
+    days = 0
+    yield start_date + datetime.timedelta(days=days)
+    days += 1
+
 def main():
-    posts_by_date = extract_date_posts()
-    print(posts_by_date)
-    
+    posts_by_day = extract_date_posts()
+    print(posts_by_day)
+
+    # start date seems to be May 2011, so let's start from 2011
+    letter = ''
+    for year in range(2011, datetime.date.today().year + 1):
+        is_leap = (year % 4) == 0
+        for month in range(1, 12+1):
+            month_length = 29 if month == 2 and is_leap else month_lengths[month]
+            print(f"\n {year} {month:2}: ", end="")
+            for day in range(1, month_length + 1):
+                date = (datetime.datetime(year, month, day)).strftime("%A, %B %-d, %Y")
+                # print(date)
+                letter = ' '
+                if date in posts_by_day:
+                    count = len(posts_by_day[date])
+                    if count > 1:
+                        letter = 'X'
+                    elif count == 1:
+                        letter = '+'
+                    else:
+                        letter = '-'
+                print(letter, end='')
+    print("\n")
 
 if __name__ == '__main__':
     main()
